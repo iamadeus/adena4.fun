07213ddd6a074fa221b31373a48e629beb3761a4
iamadeus <icorsair56@gmail.com>
2023-07-02 00:57:43 +0300

    2

---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Asterios Adena");
+?>
+chc
+    <br>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
