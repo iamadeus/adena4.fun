747f89b79dd6ee56b0675eea9efe50cd7d78cdd5
iamadeus <icorsair56@gmail.com>
2023-07-04 00:59:27 +0300

    3 (каталоги)

---
+++ Contacts/index.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Контакты");
+?>
+
++375292697073
+vk.com/w84miracle
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ about/index.php
@@ -0,0 +1,7 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("обо мне");
+?>
+
+продажа адены и кача 
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
