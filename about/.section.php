5b59abdc6155be60257822b0ae0a88a25e29ddd8
iamadeus <icorsair56@gmail.com>
2023-07-04 01:22:03 +0300

    2

---
+++ about/.section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = "обо мне";
+$arDirProperties = Array(
+   "description" => "обо мне",
+   "keywords" => "обо мне",
+   "title" => "обо мне",
+   "keywords_inner" => "обо мне"
+);
+?>
---
+++ catalog/.section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = "Адена";
+$arDirProperties = Array(
+   "description" => "адена",
+   "keywords" => "адена",
+   "title" => "адена",
+   "keywords_inner" => "адена"
+);
+?>
