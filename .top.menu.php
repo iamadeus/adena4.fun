c06a66e719887e0baf518b158dfac5f44455dfad
iamadeus <icorsair56@gmail.com>
2023-07-04 12:09:02 +0300

    04.07 \\// 12 10

---
+++ .top.menu.php
@@ -0,0 +1,32 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"На главную", 
+		"/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Адена",
+		"/catalog/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Обо мне", 
+		"/about/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Контакты", 
+		"/contacts/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
